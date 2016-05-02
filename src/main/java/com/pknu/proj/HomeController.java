4a15cc29d2300241c46e26caa5e3e5a2c1f8bfce
PKNU <PKNU@PKNU-PC>
2016-05-02 16:02:29 +0900

    test

--- src/main/java/com/pknu/proj/HomeController.java
+++ src/main/java/com/pknu/proj/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		System.out.println("helloworld");
 		model.addAttribute("serverTime", formattedDate );
-		//hkjhkjhkjhkj
+		//hkjhkjhkjhkj123
 		return "home";
 	}
 	
