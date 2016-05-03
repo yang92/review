0a59813b053dba639f0c4044355d311f243cdc47
heechun <Lee>
2016-05-03 09:44:27 +0900

    각 패키지별 클래스 생성

---
+++ src/main/java/com/pknu/proj/dto/MemberDto.java
@@ -0,0 +1,36 @@
+package com.pknu.proj.dto;
+
+public class MemberDto {
+	private String id;
+	private String password;
+	private String name;
+	private String grade;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	@Override
+	public String toString() {
+		return "MemberDto [id=" + id + ", password=" + password + ", name=" + name + ", grade=" + grade + "]";
+	}
+}
