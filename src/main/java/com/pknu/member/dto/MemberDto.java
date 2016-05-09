fbc8be029bcb5212b22c2ca9f654546d5063beb9
Koo <rngusals77@gmail.com>
2016-05-09 19:41:02 +0900

    멤버 dto 바꾸고 쿼리 바꿈. 근데 에라남

--- src/main/java/com/pknu/member/dto/MemberDto.java
+++ src/main/java/com/pknu/member/dto/MemberDto.java
@@ -10,7 +10,41 @@ public class MemberDto {
 	private String regdate;
 	private String score;
 	private String memo;
+	private String grade;
+	private String nickname;
+	private String phonenumber;
+	private String picture;
 	
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
 	public String getId() {
 		return id;
 	}
@@ -68,7 +102,8 @@ public class MemberDto {
 	@Override
 	public String toString() {
 		return "MemberDto [id=" + id + ", pw=" + pw + ", name=" + name + ", email=" + email + ", sex=" + sex
-				+ ", birth=" + birth + ", regdate=" + regdate + ", score=" + score + ", memo=" + memo + "]";
+				+ ", birth=" + birth + ", regdate=" + regdate + ", score=" + score + ", memo=" + memo + ", grade="
+				+ grade + ", nickname=" + nickname + ", phonenumber=" + phonenumber + ", picture=" + picture + "]";
 	}
 	
 	
